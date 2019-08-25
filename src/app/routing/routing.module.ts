2bd661d0eb0be44c4d22a3e0f8ebc0ec8be8d868
Robin <robinkaris7@gmail.com>
2019-08-25 18:41:16 +0300

    this commit will make changes to routing module

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {RouterModule, Routes} from "@angular/router"
+import {RouterModule, Routes} from "@angular/router";
 // import {LandComponent} from "../home/home.component";
 // import {UserComponent} from "../profile/profile.component"
 
