d2eba9059c42c5ef343f71ca30e1c6a062a6c279
Robin <robinkaris7@gmail.com>
2019-08-26 07:58:03 +0300

    this commit will modify environments.ts and git ignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '095a9eb8a6c8606943971039fcd751e4547cde5c'
 };
 
 /*
